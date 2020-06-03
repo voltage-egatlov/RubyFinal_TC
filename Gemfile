350545e05a883c8271566d0663d1dcafa7cab5d3
Tej Chhabra <tej.chhabra03@gmail.com>
2020-06-03 19:43:46 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+gem "nokogiri", "~> 1.10"
+
+gem "httparty", "~> 0.18.0"
+
+gem "pry", "~> 0.13.1"
---
+++ main.rb
@@ -0,0 +1,113 @@
+require "nokogiri"
+require "httparty"
+class Scraper
+
+  def initialize
+    doc=HTTParty.get("https://store.steampowered.com/search/?filter=weeklongdeals")
+    @parse_page ||= Nokogiri::HTML(doc);
+  end
+  def get_titles
+    elements=@parse_page.css('.search_results').css('.responsive_search_name_combined').css('.col.search_name.ellipsis').css('.title')
+  end
+  def get_prices
+    prices=@parse_page.css('.search_results').css('.responsive_search_name_combined').css('.col.search_price_discount_combined.responsive_secondrow').css('.col.search_price.discounted.responsive_secondrow')
+  end
+  def get_discounts
+    discounts=@parse_page.css('.search_results').css('.responsive_search_name_combined').css('.col.search_price_discount_combined.responsive_secondrow').css('.col.search_price.discounted.responsive_secondrow').css('strike')
+  end
+  def get_href
+    href=@parse_page.css('.search_results').css("a[href]")
+  end
+  def get_revs
+    revs=@parse_page.css('.search_results').css('.responsive_search_name_combined').css('.col.search_reviewscore.responsive_secondrow')
+  end
+
+  def option1(array)
+    puts "Enter the index of the game you would like to know more about : "
+    index=gets.chomp.to_i
+    game=array[index-1][3]
+    specific_page(game)
+  end
+
+  def specific_page(link)
+    page=HTTParty.get(link)
+    @new_page= Nokogiri::HTML(page);
+
+    description=@new_page.css(".responsive_page_frame.with_header").css(".responsive_page_content").css(".responsive_page_template_content").css(".page_content_ctn").css(".block").css(".game_background_glow").css(".block_content.page_content").css(".rightcol").css(".glance_ctn").css(".game_description_snippet").text.tr("\t","").tr("\r\n", "")
+
+    print(description)
+  end
+  
+  scraper=Scraper.new
+  discounts=scraper.get_discounts
+  titles=scraper.get_titles
+  prices=scraper.get_prices
+  hrefs=scraper.get_href
+  revs=scraper.get_revs
+
+  array_price=[]
+  array_discount=[]
+  array_title=[]
+  array_href=[]
+  array_ratings=[]
+
+  prices.each do |price|
+    price.children.each do |child|
+     if child.name == 'text' && !child.text.strip.empty?
+       array_price.push(price.text.tr(" ","").tr("\r\n", ""))
+      end
+    end
+  end
+
+  discounts.each do |discount|
+    array_discount.push(discount.text)
+  end
+
+  titles.each do |title|
+    array_title.push(title.text)
+  end
+
+  hrefs.each do |href|
+    array_href.push(href["href"])
+  end
+
+  revs.each do|rev|
+    rating=((rev.css('.search_rqeview_summary.mixed').attr('data-tooltip-html').to_s) + (rev.css('.search_review_summary.positive').attr('data-tooltip-html').to_s) + (rev.css('.search_review_summary.negative').attr('data-tooltip-html').to_s)).tr("abcdefghijklmnopqrstuvwxyz<> ABCDEFGHIJKLMNOPQRSTUVWXYZ.","").split("%")
+    
+    array_ratings.push(rating)
+  end
+
+  array=[]
+
+  (0...array_title.size).each do |i|
+    array_element=[]
+    array_element.push(array_title[i])
+    array_element.push(array_price[i])
+    array_element.push(array_discount[i])
+    array_element.push(array_href[i])
+    array_element.push(array_ratings[i])
+    array.push(array_element)
+  end
+
+  print("Here are the games on sale right now:\n")
+  (0...array.size).each do |i|
+    array[i][1].slice!(array[i][2])
+    array[i][1]
+    puts "#{i+1}: #{array[i][0]} - #{array[i][1]} - usually #{array[i][2]} - rating #{array[i][4][0]}% positive with #{array[i][4][1]} voters"
+  end
+  #puts array
+  choice =0
+  while choice!=-1
+    puts "Menu"
+    puts "1. Open description of game"
+    puts "-1. Quit"
+    print("Enter choice: ")
+    choice=gets.chomp.to_i
+    if(choice==1)
+      scraper.option1(array)
+    end
+  end
+end
+
+
+#@parse_page.css('.search_results').css('.responsive_search_name_combined').css('.col.search_reviewscore.responsive_secondrow').css('.search_review_summary.mixed').first.attr('data-tooltip-html')
